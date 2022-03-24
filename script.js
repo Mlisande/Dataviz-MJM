7d31e87c36abca81dc2aa6793d13d91c3adf2787
JacquelineKY <j.keren-yuen@outlook.com>
2022-03-24 16:52:23 +0100

    journal de bord, recherche stringify et parse

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function refresh(){
+/*function refresh(){
     var t = 1000; // rafraîchissement en millisecondes
     setTimeout('showDate()',t);
 }
@@ -16,7 +16,34 @@ function showDate() {
  
  fetch("https://superheroapi.com/api/access-token/character-id/image")
 .then(reponse =>reponse.json())
-.then(reponse2 => console.table(reponse2))
+.then(reponse2 => console.table(reponse2))*/
 
+/*import * as Dataviz from './data.json';
 
+function load() {
+ var mydata = JSON.parse(Dataviz);
+ alert(mydata[0].lundi);
+ alert(mydata[1].mardi);
+ alert(mydata[2].mercredi);
+ alert(mydata[3].jeudi);
+ alert(mydata[4].vendredi);
+}
 
+load();*/
+
+/*let myJSON = {
+    "lundi":["satisfait"],
+    "mardi":["content"],
+    "mercredi":["neutre"],
+    "jeudi":["content", "fatigué"],
+    "vendredi":["festif"]
+};
+
+//let myNewJSON = JSON.stringify(myJSON,null, '\t');
+
+console.log(myJSON.jeudi);*/
+
+let myJSON = '{"lundi":"satisfait", "mardi":"content","mercredi":"neutre","jeudi":["content", "fatigué"],"vendredi":"festif"}';
+
+let myNewJSON = JSON.parse(myJSON);
+console.log(myNewJSON.lundi);
